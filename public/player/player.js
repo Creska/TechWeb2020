2ad6efb34b30a98657d8a55aa89fab5dc58b2efe
cguidotti <46874835+cguidotti@users.noreply.github.com>
2020-08-06 23:43:02 +0200

    continuato il lavoro col player

--- public/player/player.js
+++ public/player/player.js
@@ -1,52 +1,38 @@
-/*
-NOTE:
-* Per semplicità, l'applicazione internamente identifica le quest e le attività il base all'indice che esse hanno nell'array di cui fanno parte.
-* La sintassi di dichiarazione dell'oggetto JSON non è formalmente corretta ma funziona comunque su Chrome.
-*/
-
 /* Riassunto del funzionamento:
 Questo è solo un test, quindi presenta vari bug che bisogna evitare conoscendo il funzionamento di questo mini-programma.
 Alla schermata dei settings viene richiesto di scegliere una delle due opzioni (evitare di cliccarle entrambe o nessuna). A seconda dell'opzione scelta, cambierà l'attività successiva alla prima.
+NOTE:
+* Per semplicità, l'applicazione internamente identifica le quest e le attività il base all'indice che esse hanno nell'array di cui fanno parte.
 */
 
-/* ROBA DI DEBUGGING
-var example = {
-	"game_html": "<div id='game'> <div id='storytitle'>Titolo della storia</div> </div>",
-	"settings": {},
-	"settings_form": "<div id='settingsform'><div>Scegli una delle due opzioni:</div><label for='optionchoice1'>Prima</label><input type='checkbox' id='optionchoice1' name='optionchoice1' class='formoption'><br><label for='optionchoice2'>Seconda</label><input type='checkbox' id='optionchoice2' name='optionchoice2' class='formoption'><br><button id='submitsettings' onclick='$(\"#settingsform\").children().each(function(){if($(this).attr(\"class\")==\"formoption\"){if($(this).is(\":checked\"))story.settings[$(this).attr(\"id\")]=1;else story.settings[$(this).attr(\"id\")]=0;}}); goToQuest(0);'>Salva</button></div>",
-	"quests": [
+var Storia = {
+	ACCESSIBILITY: 0,
+	story_title: "<div id='StoryTitle'>Storia di prova</div>",
+	story_ID: -1,
+	game_mode: "Singleplayer",
+	quests: [
 		{	
-			"quest_html": "<div id='quest0' class='quest'> <div id='questtitle'>Prima quest</div> </div>",
-			"activities": [
+			quest_title: "<div class='QuestTitle'>Prima quest</div>",
+			activities: [
 				{
-					"activity_text": "<div id='activity0' class='activity'> <p>Questa è la prima attività.<br>Clicca su PROSEGUI per andare alla seconda.</p> <button id='activity0answerfield' class='answerfield' onclick='if (story.settings.optionchoice1) goToActivity(1); else goToActivity(2);'>Prosegui</button> </div>",
-					"right_answer": "",
-					"need_human_evaluation": false,
-					"action_on_activity_answer": "goToActivity(1);" // ignorare questo campo
-				},
-				{
-					"activity_text": "<div id='activity1' class='activity'> <p>Questa è la seconda attività. Avevi scelto la prima opzione.<br>Clicca su FINE per completare il test.</p> <button id='activity1answerfield' class='answerfield' onclick='window.alert(\"Test andato a buon fine\");'>Fine</button> </div>",
-					"right_answer": "",
-					"need_human_evaluation": false,
-					"action_on_activity_answer": "window.alert('Test andato a buon fine');" // ignorare questo campo
-				},
-				{
-					"activity_text": "<div id='activity2' class='activity'> <p>Questa è la terza attività. Avevi scelto la seconda opzione.<br>Clicca su FINE per completare il test.</p> <button id='activity1answerfield' class='answerfield' onclick='window.alert(\"Test andato a buon fine\");'>Fine</button> </div>",
-					"right_answer": "",
-					"need_human_evaluation": false,
-					"action_on_activity_answer": "window.alert('Test andato a buon fine');" // ignorare questo campo
+					activity_text: "<div class='ActivityText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div><div class='AnswerField'></div>",
+					answer_field: "<div class='AnswerField_Description>Inserire la risposta</div><div class='AnswerField_Input'><input type='text'></div>",
+					right_answer: "",
+					answer_score: "",
+					ASK_EVAL: 0,
+					expected_time: 0
 				}
 			]
 		}
 	],
-	"stylesheet": "",
-	"score": []
+	stylesheet: "",
+	score: []
 };
 
-var story = JSON.parse( JSON.stringify( example ) );
-*/
+// bottone standard per passare alla prossima attività - da completare. scopo: durante la fase di costruzione dle gioco, il player lo inserisce alla fine di ogni attività
+var NextActivityButtonTemplate = "<button class='NextActivity'>PROSEGUI</button>";
 
-var story; // conains the current story
+
 var socket; // contains the socket for this specific player
 
 function validateInput(answer, current_quest, current_activity) {
@@ -96,53 +82,57 @@ $(function () {
 			//TODO right input handling
 		}
 	});
+
 	// indica il numero della quest attiva ed il numero dell'attività attiva
-	var currentStatus = {
-		currentQuest: -1,
-		currentActivity: -1
+	var CurrentStatus = {
+		QuestN: -1,
+		ActivityN: -1
 	};
+
+	$( "#StartScreen" ).prepend( Storia.story_title );
 
 	/**
 		* funzione utilizzata per gestire le condizioni di errore all'interno del player
 		* in generale sarebbe meglio fermare l'applicazione e basta
 		*/
 	function handleError() {
-		window.alert("!!! MAJOR ERROR !!!");
+		window.alert( "!!! MAJOR ERROR !!!" );
 	};
 
-	function goToActivity(activity_n) {
-		if (activity_n) {
-			$("#activity" + String(currentStatus.currentActivity)).replaceWith(story.quests[currentStatus.currentQuest].activities[activity_n].activity_text);
-		}
-		else {
-			$("#quest" + String(currentStatus.currentQuest)).append(story.quests[currentStatus.currentQuest].activities[activity_n].activity_text);
-		}
+	function goToActivity( activity_n ) {
+		CurrentStatus.ActivityN = activity_n;
 
-		currentStatus.currentActivity = activity_n;
+		let NewActivity = Storia.quests[CurrentStatus.QuestN].activities[activity_n].activity_text;
+		NewActivity.attr( "id", "Quest" + String( CurrentStatus.QuestN ) + "Activity" + String( activity_n ) + "Text" );
+
+
 	};
 
-	function goToQuest(quest_n) {
-		if (quest_n) {
-			$("#quest" + String(currentStatus.currentQuest)).replaceWith(story.quests[quest_n].quest_html);
-		}
-		else {
-			if (document.getElementById("settingsform") == null) window.alert("errore"); // debugging
-			$("#settingsform").replaceWith(story.quests[0].quest_html);
-		}
 
-		currentStatus.currentQuest = quest_n;
+	/**
+	 * @param quest_n
+	 * Carica a schermo la quest specificata. Svuota il container della quest e ci aggiunge il titolo nuovo. Poi carica la quest numero zero.
+	 */
+	function goToQuest( quest_n ) {
+		CurrentStatus.QuestN = quest_n;
 
-		goToActivity(0); // per convenzione ogni quest inizia con l'attività zero --> facciamo così???
+		let Container = $( ".Quest" );
+		Container.empty();
+
+		Container.append( Storia.quests[quest_n].quest_title );
+		$( ".QuestTitle" ).attr( "id", "Quest" + String( quest_n ) + "QuestTitle" );
+
+		goToActivity( 0 );
 	};
 
-	function goToSettings() {
-		$("#game").append(story.settings_form);
-	};
 
+	/**
+	 * Semplicemente avvia il gioco sostituendo alla schermata di benvenuto il container "Quest"
+	 */
 	function startGame() {
-		$("#welcome").replaceWith(story.game_html);
+		$( "#StartScreen" ).replaceWith( $( "template" ).content );
 
-		goToSettings(); // invio alla schermata dei settings, che precede la quest 0
+		goToQuest( 0 );
 	};
 
 });

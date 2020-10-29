084250a22b7d3ee9c61499ad93cda1f6deaaf538
cguidotti <46874835+cguidotti@users.noreply.github.com>
2020-10-29 20:34:25 +0100

    editor - ridisegnate sezioni di modifica answer field e outcomes

--- game_example.js
+++ game_example.js
@@ -27,10 +27,19 @@ game_example = {
 			activities: [
 				{
 					activity_text: [], // testo HTML dell'attività. è un array in cui ogni elemento è un testo/immagine/galleria
-					answer_field: "", // HTML del campo risposta
-					right_answer: "", // contiene la risposta giusta
-					answer_score: "", // punteggio per la risposta giusta
-					answer_outcome: {}, // oggetto nel quale ad ogni entry corrisponde uno dei possibilit outcomes, ed il valore equivale all'attività a cui spostarsi
+					activity_type: "", // con READING indica attività di sola lettura, con ANSWER indica la presenza del campo risposta
+					answer_field: {
+						description: "", // eventuale indicazione per il giocatore
+						type: "", // tipo dell'answer field: checklist, number o text
+						options: [] // array di opzioni nel caso type = checklist
+					},
+					answer_outcome: [
+						{
+							response: "", // possibile risposta
+							nextquest: false, // indica se conduce alla prossima quest
+							nextactivity: "" // indica l'attività a cui conduce
+						}
+					],
 					ASK_EVAL: 0, // flag che indica se c'è necessità di valutazione umana per la risposta
 					GET_CHRONO: 0, // flag che indica se bisogna calcolare il tempo di risposta alla domanda
 					expected_time: 0, // tempo in ms previsto per il completamento dell'attività

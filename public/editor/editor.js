9f4f91799334c8ec1420a400ca15a160cc665e2e
maomettino <aymentragha@gmail.com>
2020-11-08 13:54:01 +0100

    flexy is sexy

--- public/editor/editor.js
+++ public/editor/editor.js
@@ -1081,4 +1081,17 @@ function closeEditor() {
 }
 
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 
